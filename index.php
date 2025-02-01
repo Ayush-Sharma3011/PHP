af553636b1a140773244fb2123a025b5de6970cd
Ayush-Sharma3011 <ayush.sharma235@lpu.in>
2025-02-01 21:00:00 +0530

    php practice

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 $car = array(1,2,3,"SUN");
 // var_dump($car);
 echo "\n";
-echo $car[3];
+// echo $car[0];
+echo "\n";
+array_push($car, 4);
+// $car[0] = 4;
+
+foreach($car as $value){
+    echo "\n";
+    echo $value;
+}
+
 
 ?>
