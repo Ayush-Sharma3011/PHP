2ec24367eea2151b9ee65acac72f632b689b00da
Ayush-Sharma3011 <ayush.sharma235@lpu.in>
2025-02-10 12:42:49 +0530

    oops

--- function/index.php
+++ function/index.php
@@ -1,3 +1,25 @@
- <?php
- 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
+<body class="item-center justify-center">
+    <div class="border-2 m-[10px] p-[10px]  ">
+        <?php 
+
+function multiplyByTwo(float $number): float {
+    $number *= 1.5;
+    return $number;
+}
+
+$originalNumber = 5.5;
+$newNumber = multiplyByTwo($originalNumber);
+
+echo "Original number: " . $originalNumber . "<br>";
+echo "New number: " . $newNumber;
+?></div>
+</body>
+</html>
